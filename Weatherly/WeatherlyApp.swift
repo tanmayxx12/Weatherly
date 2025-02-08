df2fd9ba1086736e839d4ad3e25d7de94a8c8ec6
tanmayxx12 <troy12rollsza@gmail.com>
2025-02-08 16:56:37 +0530

    Initial Commit

---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Tanmay . on 08/02/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
